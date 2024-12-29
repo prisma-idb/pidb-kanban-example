beff6e677a8eb156166a234fede1c91e1cf4fc81
WhyAsh5114 <whyash5114@gmail.com>
2024-12-29 18:58:41 +0530

    ui: theming and font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
+	import ModeToggle from '$lib/components/mode-toggle.svelte';
+	import { ModeWatcher } from 'mode-watcher';
 	import '../app.css';
+
 	let { children } = $props();
 </script>
+
+<ModeWatcher />
+<ModeToggle />
 
 {@render children()}
