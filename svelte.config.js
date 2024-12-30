96bb0c718bff1d745f503a7f03149a65f09d85e3
WhyAsh5114 <whyash5114@gmail.com>
2024-12-30 19:47:14 +0530

    feat: add GitHub Actions workflow for deployment to GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -18,6 +18,9 @@ const config = {
 		serviceWorker: { register: false },
 		alias: {
 			$routes: './src/routes'
+		},
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
